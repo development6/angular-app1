0a38e12bcb2a7a52bba210263dbeba4264d656a1
israel palma <ipalm001@gmail.com>
2021-09-28 10:41:56 -0300

    Creacion inicial de interfaces

---
+++ src/app/interfaces/categoria.interface.ts
@@ -0,0 +1,12 @@
+export interface Categoria {
+    frutas: string;
+    veduras: string;
+    lacteos: string;
+    cereales: string;
+    pescados: string;
+    carne: string;
+    huevos: string;
+    legumbres: string;
+    frutos_secos: string;
+
+}
--- src/app/services/categoria.service.ts
+++ src/app/services/categoria.service.ts
@@ -10,17 +10,29 @@ export class CategoriaService {
   constructor(private http: HttpClient) { 
     console.log("Constructor CategoriaService");
     this.cargarCategorias();
+    //this.cargarCategoriasWeb();
   }
 
+  //estructura base this.http.get('').subscribe( (respuesta) => {});
   private cargarCategorias(){
     console.log("Metodo privado CargarCategorias");
 
-    //estructura base this.http.get('').subscribe( (respuesta) => {});
-    this.http.get('assets/data/productos.json').subscribe( (respuesta) => {
-      console.log(respuesta);
+    //lectura de archivo json local
+    this.http.get('assets/data/categorias.json')
+    .subscribe( (respuesta: any) => {
+      console.log("Respuesta: ",respuesta);
+      console.log("Categorias: ",respuesta['frutas']);
 
     });
 
   }
 
+  private cargarCategoriasWeb(){
+
+    this.http.get('https://learsixela.github.io/productos_json/productos.json')
+    .subscribe( (respuesta) => {
+      console.log("Respuesta desde la web: ",respuesta);
+      //console.log("Productos: ",respuesta['productos']);
+    });
+  }
 }

552b6bc54e2d68d485f18d2ead7578cd159e9b4c
israel palma <ipalm001@gmail.com>
2021-09-28 10:56:44 -0300

    vincualr interfaz con el servicio

--- src/app/interfaces/categoria.interface.ts
+++ src/app/interfaces/categoria.interface.ts
@@ -1,12 +1,12 @@
 export interface Categoria {
-    frutas: string;
-    veduras: string;
-    lacteos: string;
-    cereales: string;
-    pescados: string;
-    carne: string;
-    huevos: string;
-    legumbres: string;
-    frutos_secos: string;
+    frutas?: string;
+    veduras?: string;
+    lacteos?: string;
+    cereales?: string;
+    pescados?: string;
+    carne?: string;
+    huevos?: string;
+    legumbres?: string;
+    frutos_secos?: string;
 
 }
--- src/app/services/categoria.service.ts
+++ src/app/services/categoria.service.ts
@@ -1,30 +1,42 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Categoria } from '../interfaces/categoria.interface';
 
 
 @Injectable({
   providedIn: 'root'
 })
 export class CategoriaService {
+  //declare variables
+  categoria: Categoria = {};
+
   //ng g s services/categoria
   constructor(private http: HttpClient) { 
     console.log("Constructor CategoriaService");
     this.cargarCategorias();
-    //this.cargarCategoriasWeb();
+    this.cargarCategorias_old();
   }
 
   //estructura base this.http.get('').subscribe( (respuesta) => {});
+  private cargarCategorias_old(){
+
+    //lectura de archivo json local
+    this.http.get('assets/data/categorias.json')
+    .subscribe( (respuesta: any) => {
+
+      console.log("Respuesta: ",respuesta);
+      console.log("Categorias: ",respuesta['frutas']);
+    });
+  }
   private cargarCategorias(){
     console.log("Metodo privado CargarCategorias");
 
     //lectura de archivo json local
     this.http.get('assets/data/categorias.json')
-    .subscribe( (respuesta: any) => {
-      console.log("Respuesta: ",respuesta);
-      console.log("Categorias: ",respuesta['frutas']);
-
+    .subscribe( (respuesta: Categoria) => {
+      this.categoria = respuesta;
+      console.log("Respuesta: ",this.categoria);
     });
-
   }
 
   private cargarCategoriasWeb(){

e67f4fe370d0475dfdadff087582ce5777c977f6
israel palma <ipalm001@gmail.com>
2021-09-29 17:11:04 -0300

    se cambian componentes por modulos

--- src/app/general/general.module.ts
+++ src/app/general/general.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 import { ModalComponent } from './modal/modal.component';
 import { PruebasComponent } from './pruebas/pruebas.component';
 import { FormsModule } from '@angular/forms';
+import { CategoriaComponent } from './categoria/categoria.component';
 
 
 
 @NgModule({
   declarations: [
+    CategoriaComponent,
     ModalComponent,
     PruebasComponent
   ],
